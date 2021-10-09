7d96cdec352de0cd10e2c3c54fd85935cdae1175
Corentin PAPE <corentin.pape@epita.fr>
2021-10-10 01:14:46 +0200

    init

---
+++ home.nix
@@ -0,0 +1,51 @@
+{ pkgs, lib, ... }:
+
+{
+  home.username = "coco";
+  home.homeDirectory = "/home/coco";
+  home.packages = [
+  ];
+
+  programs.firefox = {
+    enable = true;
+  };
+
+  programs.home-manager = {
+    enable = true;
+    path = "…";
+  };
+
+  programs.bash = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Corentin PAPE";
+    userEmail = "corentin.pape@epita.fr";
+  };
+
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    oh-my-zsh = {
+      enable = true;
+    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        file = "powerlevel10k.zsh-theme";
+        src = pkgs.zsh-powerlevel10k;
+      }
+    ];
+    initExtra = ''
+      source "/home/coco/.config/zsh/plugins/powerlevel10k/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
+      source "/home/coco/.config/zsh/.p10k.zsh"
+   '';
+  };
+
+  home.stateVersion = "21.05";
+}
