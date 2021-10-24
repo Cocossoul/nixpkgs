4594ddd61c76e55ecb3237ba9c17b32eb3327768
Corentin PAPE <corentin.pape@epita.fr>
2021-10-24 22:21:17 +0200

    separated zsh.nix file

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ ... }:
 
 {
   home.username = "coco";
@@ -25,32 +25,11 @@
     userEmail = "corentin.pape@epita.fr";
   };
 
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    dotDir = ".config/zsh";
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    oh-my-zsh = {
-      enable = true;
-    };
-    plugins = [
-      {
-        name = "powerlevel10k";
-        file = "powerlevel10k.zsh-theme";
-        src = pkgs.zsh-powerlevel10k;
-      }
-    ];
-    initExtra = ''
-      source "/home/coco/.config/zsh/plugins/powerlevel10k/share/zsh-powerlevel10k/powerlevel10k.zsh-theme"
-      source "/home/coco/.config/zsh/.p10k.zsh"
-      set -o vi
-   '';
-  };
-
   imports = [
     ./i3.nix
     ./picom.nix
+    ./zsh.nix
+    ./vim.nix
   ];
 
   home.stateVersion = "21.05";
